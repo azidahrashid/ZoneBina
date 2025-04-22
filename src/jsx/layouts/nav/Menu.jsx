b96f8aeedfd658294492f7717283169353466ef6
azidahrashid <turnkeyjie@gmail.com>
2025-04-22 13:37:42 +0800

    update menu jsx
    add pages

--- src/jsx/layouts/nav/Menu.jsx
+++ src/jsx/layouts/nav/Menu.jsx
@@ -14,5 +14,44 @@ export const MenuList = [
             
         ],
     },
+        //Pages
+        {
+            title:'Pages',
+            classsChange: 'mm-collapse',
+            iconStyle: <i className="material-icons">article</i>,
+            content : [
+                {
+                    title:'Error',
+                    hasMenu : true,
+                    content : [
+                        {
+                            title: 'Error 400',
+                            to : 'page-error-400',
+                        },
+                        {
+                            title: 'Error 403',
+                            to : 'page-error-403',
+                        },
+                        {
+                            title: 'Error 404',
+                            to : 'page-error-404',
+                        },
+                        {
+                            title: 'Error 500',
+                            to : 'page-error-500',
+                        },
+                        {
+                            title: 'Error 503',
+                            to : 'page-error-503',
+                        },
+                    ],
+                },
+                {
+                    title:'Lock Screen',
+                    to: 'page-lock-screen',
+                },
+    
+            ]
+        },
    
 ]
